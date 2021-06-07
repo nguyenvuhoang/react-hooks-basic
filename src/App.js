7fa9c49db7c4e8d54557cd87d38424717f3355de
nguyenvuhoang <nguyenvuhoangz@gmail.com>
2021-06-07 21:34:27 +0700

    First commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.scss';
+import React from 'react'
+function App() {
+    return (
+        <div className="app">
+            <h1>HELLO WORLD</h1>
+        </div>
+    );
+}
+
+export default App;
